c7e85ec7ab09a34e15d2abba24f58fddd0b47fd4
DV09developer <divyeshvoriya3@gmail.com>
2026-05-10 12:19:14 +0530

    Rermove underline from the Link tag

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ url , buttonText , link} : ButtonProps) {
   return (
     <div className={`flex z-2 items-center gap-[4px] p-[10px] ${buttonText === "Instant Lottery" ? "bg-[#96152B] border-1 rounded-[10px] border-[#FFFFFF33]" : "rounded-[10px]"}`}>
         <img src={url} alt={buttonText}  />
-        <Link to={link} className="text-[#FFFFFF] text-[14px] font-[700]">
+        <Link to={link} className="text-[#FFFFFF] text-[14px] font-[700] no-underline">
             {buttonText}
         </Link>
     </div>
